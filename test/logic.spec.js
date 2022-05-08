cc8047ceb5363a6be973d7de3096e43225000cc5
Chris Demetriad <chris@demetriad.co.uk>
2022-05-08 18:05:52 +0100

    Adding unit tests

---
+++ test/logic.spec.js
@@ -0,0 +1,91 @@
+import { game } from "../src/js/game.js";
+import { setMode, restart, setType, getTypes, getWinner, setScore, getRandomSelection } from "../src/js/logic";
+
+beforeEach(() => {
+	game.jsDom = false;
+	restart();
+});
+
+test("Should start with score from 0", () => {
+	expect(game.players[0].score).toBe(0);
+	expect(game.players[1].score).toBe(0);
+});
+
+test("setMode() should change the game mode", () => {
+	let event = {
+		target: {
+			textContent: "manual",
+		},
+	};
+	setMode(event);
+	expect(game.mode).toBe("manual");
+
+	event = {
+		target: {
+			textContent: "auto",
+		},
+	};
+	setMode(event);
+	expect(game.mode).toBe("auto");
+});
+
+test("setType() should correctly set the game type", () => {
+	let event = {
+		target: {
+			textContent: "basic",
+		},
+	};
+	setType(event);
+	expect(game.type).toBe("basic");
+	expect(getTypes()).toStrictEqual({
+		ROCK: ["SCISSORS"],
+		PAPER: ["ROCK"],
+		SCISSORS: ["PAPER"],
+	});
+
+	event = {
+		target: {
+			textContent: "extended",
+		},
+	};
+	setType(event);
+	expect(game.type).toBe("extended");
+	expect(getTypes()).toStrictEqual({
+		ROCK: ["SCISSORS", "LIZARD"],
+		PAPER: ["ROCK", "SPOCK"],
+		SCISSORS: ["PAPER", "LIZARD"],
+		LIZARD: ["SPOCK", "PAPER"],
+		SPOCK: ["SCISSORS", "ROCK"],
+	});
+});
+
+test("getRandomSelection() should return a string match", () => {
+	expect(getRandomSelection()).not.toBeUndefined();
+	expect(getRandomSelection()).toMatch(/ROCK|PAPER|SCISSORS|LIZARD|SPOCK/);
+});
+
+// types
+test("getWinner() should get the right value", () => {
+	expect(getWinner("ROCK", "SCISSORS")).toBe("win");
+	expect(getWinner("PAPER", "PAPER")).toBe("tie");
+	expect(getWinner("SCISSORS", "ROCK")).toBe("lose");
+	game.type = "extended";
+	expect(getWinner("ROCK", "SPOCK")).toBe("lose");
+	expect(getWinner("PAPER", "LIZARD")).toBe("lose");
+	expect(getWinner("SCISSORS", "LIZARD")).toBe("win");
+	expect(getWinner("LIZARD", "LIZARD")).toBe("tie");
+	expect(getWinner("SPOCK", "SCISSORS")).toBe("win");
+});
+
+test("setScore() should increment score", () => {
+	const player1 = game.players[0];
+	const player2 = game.players[1];
+
+	setScore(player1);
+	expect(player1.score).toBe(1);
+	expect(player2.score).toBe(0);
+
+	setScore(player1);
+	expect(player1.score).toBe(2);
+	expect(player2.score).toBe(0);
+});
